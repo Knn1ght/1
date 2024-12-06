f3403eacac4ce87e3b4b04fc9509abcc8b436905
Knn1ght <andersensup1990@gmail.com>
2024-12-06 09:34:18 +0100

    first commit

---
+++ Tamagochi/Dog.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagochi
+{
+    public class Dog : pet
+    {
+        public override string WhoamI()
+        {
+            return "Dog";
+        }
+        //public int Hunger { get; private set; } = 30; equal to underneath
+        private int hunger = 50;
+        public int Hunger
+        {
+         get
+            {
+                return hunger; 
+            }
+            set 
+            {
+                hunger = value;
+
+                if (!Isalive())
+                {
+                    throw new DeadPetException();
+                }
+            }
+        }
+        public int Boredom { get; private set; } = 50;
+        public void Feed()
+        {
+            Hunger -= 30;
+            Boredom += 15;
+            Console.WriteLine("Feeding");
+        }
+        public void Play()
+        {
+            Hunger += 30;
+            Boredom -= 25;
+            Console.WriteLine("Playing");
+        }
+        public void Chill()
+        {
+            Hunger += 10;
+            Boredom += 10;
+            Console.WriteLine("Chillin...");
+        }
+        private Random random = new Random();
+        public int Do()
+        {
+            var toss = random.Next(1, 20);
+
+            if (toss < 10)
+            { Chill(); }
+
+            else if (toss < 16)
+            { Play(); }
+
+            else
+            { Feed(); }
+            return toss;
+        }
+
+        public bool Isalive()
+        {
+            if (Hunger > 100 || Hunger < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+}
---
+++ Tamagochi/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks.Dataflow;
+using Tamagochi;
+try
+{
+    var dog = new Dog();
+    //new - new object then name
+    var who = dog.WhoamI();
+    Console.WriteLine(who);
+
+    var dog2 = new Dog();
+    
+
+    while (true)
+    {
+        var toss = dog.Do();
+
+        Console.WriteLine($"Hunger is {dog.Hunger}");
+        Console.WriteLine($"Boredom is {dog.Boredom}");
+
+        Console.WriteLine($"Toss was {toss}");
+
+        if (toss > 10)
+        {
+            var action = Console.ReadLine();
+            if (action == "F")
+            {
+                dog.Feed();
+            }
+            if (action == "P")
+            {
+                dog.Play();
+
+            }
+            while (action != "exit");
+            break;
+        }
+
+
+        Console.WriteLine();
+
+        Thread.Sleep(500);
+    }
+}
+catch (DeadPetException)
+{ Console.WriteLine("SHAME - PET IS DEAD"); }
+
+
+
